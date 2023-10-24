578a1f8d889fa18675bb236a1fb9400e57d7f3c7
lucasuchoa98 <lucasuchoalg@gmail.com>
2023-10-24 16:54:31 -0300

    mta coisa

--- relatoriosete/run.py
+++ relatoriosete/run.py
@@ -1,3 +1,6 @@
+## Alto da boa vista e Parque Maceió
+
+
 #Importando as bibliotecas padrões
 from datetime import date
 from dateutil.relativedelta import relativedelta
@@ -62,12 +65,11 @@ def filter_by_date(dataframe:pd.DataFrame, ano_filtro:str, mes_filtro :str, data
         
         # Converter a coluna "Data" para o formato de data
         dataframe['Data'] = pd.to_datetime(dataframe['Data'])
-        print(dataframe['Data'])
 
         # Filtrar o DataFrame a partir de uma data específica
         #data_filtro = pd.to_datetime(data_filtro)
         result = dataframe[(dataframe['Data'].dt.month == int(mes_filtro)) & (dataframe['Data'].dt.year == int(ano_filtro))]
-        
+        print(result)
         #result = dataframe[dataframe['Data'].dt.strftime('%Y-%m') == date].copy()
     
     #print('Tabela de atributos filtrado pela data com sucesso!')
@@ -144,6 +146,7 @@ def cap4_text(dataframe:pd.DataFrame, cliente:str) -> str:
     parametros_fora = ""
     df_copy = dataframe.copy()
 
+    print('passei aqui')
     for parametro, resultado in zip(df_copy.Parâmetro, df_copy.Resultado):
         
         if parametro.lower() == 'ph':
@@ -179,19 +182,29 @@ def cap4_text(dataframe:pd.DataFrame, cliente:str) -> str:
     else:
         
         texto1 = f'Os resultados de {parametros_fora} não atenderam as especificações dispostas na Resolução CONAMA n° 430/2011. '
-    
+    print('passei aqui2')
     ef_remo = filter_by_param(df_copy,param="Eficiência")
+    print('passei aqui3')
     ef_remo = ef_remo['Resultado'].tolist()[0]
+    print('passei aqui4')
     ef_remo_str = str(ef_remo).replace(".",",")
+    print('passei aqui5')
     dbo = filter_by_param(df_copy, param="DBO")
+    print('passei aqui6')
     dbo_saida = filter_by_ponto(dbo, ponto='Saída')
+    print('passei aqui7')
     dbo_saida = dbo_saida['Resultado'].tolist()[0]
+    print('passei aqui8')
     dbo_saida_str = str(dbo_saida)
+    print('passei aqui9')
     dbo_entrada = filter_by_ponto(dbo, ponto='Entrada')
+    print('passei aqui10')
     dbo_entrada = dbo_entrada['Resultado'].tolist()[0]
-    dbo_entrada_str = str(dbo_entrada)       
+    print('passei aqui11')
+    dbo_entrada_str = str(dbo_entrada)
+    print('passei aqui12')     
     texto2 = f"A eficiência de remoção de matéria orgânica apresentada no processo foi de {ef_remo_str}%"
-    
+    print('passei aqui13')
     if ef_remo >=60:
         
         texto3= f", valor consideravelmente acima dos 60% de eficiência de remoção estabelecido pela resolução supracitada."
@@ -204,7 +217,9 @@ def cap4_text(dataframe:pd.DataFrame, cliente:str) -> str:
         texto4 = f"Observa-se também que a DBO de entrada na ETE foi {dbo_entrada_str } mg/L, valor que pode ser considerado baixo para um efluente sanitário de acordo com a literatura e parâmetros do projeto da ETE do {cliente}."
         texto5 = f"Dessa forma, considera-se que a eficiência de remoção da matéria orgânica na ETE está subestimada."
     
-    return texto1+texto2+texto3+texto4+texto5
+    result = texto1+texto2+texto3+texto4+texto5
+    print(result)
+    return result
     
 def cap_5_text(dataframe:pd.DataFrame, cliente: str) -> str:
     """Fornece um texto a partir da tabela atributos filtrada por cliente e data
@@ -283,29 +298,19 @@ def render_html(df1:pd.DataFrame, df2:pd.DataFrame, cap2_text:str, cap4_text:str
     filename = os.path.join(root, 'html', f'{cliente}.html')
     
     with open(filename,'w') as fh:
-        
         fh.write(template.render(
-            cliente=cliente,
-            colunas1=df1.columns.to_list(),
-            colunas2=['Razão Social:', 'CNPJ:', 'Endereço:'],
-            data=data,
-            ano=ano,
-            df1 = df1.to_dict(orient='records'),
-            df2 = df2.to_dict(orient='records'),
-            cap2_text = cap2_text,
-            cap4_text = cap4_text,
-            cap5_text = cap5_text,
-            hoje = hoje
+            cliente=cliente,colunas1=df1.columns.to_list(), colunas2=['Razão Social:', 'CNPJ:', 'Endereço:'],
+            data=data, ano=ano,df1 = df1.to_dict(orient='records'),
+            df2 = df2.to_dict(orient='records'), cap2_text = cap2_text, cap4_text = cap4_text,
+            cap5_text = cap5_text, hoje = hoje
         ))
         
     if cap4_text!="" and cap5_text!="":
-        
         print(f"Gerando PDF {cliente} ... ")
         pdf_path = f'Relatório Mensal ({data}) - {cliente}.pdf'    
         html2pdf(filename, pdf_path)
         
     else:
-        
         print(f"{cliente} com problemas nos textos do cap4 e cap5")
     
 
@@ -315,8 +320,8 @@ def html2pdf(html_path:str, pdf_path:str):
     """
     options = {
         'page-size': 'Letter', 'margin-top': '0.30in', 'margin-right': '0.75in',
-        'margin-bottom': '0.75in', 'margin-left': '0.75in', 'encoding': "UTF-8",
-        'footer-font-size':'6', 'footer-font-name':'cambria', 'footer-spacing':'0.5',
+        'margin-bottom': '0.5in', 'margin-left': '0.75in', 'encoding': "UTF-8",
+        'footer-font-size':'6', 'footer-font-name':'cambria', 'footer-spacing':'6',
         'footer-center': 'ANALISE AMBIENTAL, SOLUÇÕES EM MEIO AMBIENTE\nRua R, nº 14, Quadra 9 Lote 14, Cidade Universitária, Maceió - AL\nCNPJ: 23.049.977/0001-07',
         'no-outline': None, 'enable-local-file-access': None
     }
@@ -326,7 +331,7 @@ def html2pdf(html_path:str, pdf_path:str):
 
         pdfkit.from_file(f, pdf_path, configuration=config, options=options)
         
-def main(filePath:str, sheet_name:str, original_df: pd.DataFrame, clientes_list: list, cliente:str, mode: int, ano:str, mes:str, data_relatorio:str):
+def main(filePath:str, sheet_name:str, original_df:pd.DataFrame, clientes_list:list, cliente:str, mode:int, ano:str, mes:str, data_relatorio:str):
     
     df_copy = original_df.set_index('Cliente').copy()
         
@@ -336,7 +341,7 @@ def main(filePath:str, sheet_name:str, original_df: pd.DataFrame, clientes_list:
         print(f"Unexpected {err=}, {type(err)=}")
         raise
     try:
-        iter_df = filter_by_date(iter_df_client_filtered, ano_filtro=ano, mes_filtro=mes, data_filtro=data_relatorio)
+        iter_df = filter_by_date(iter_df_client_filtered, ano_filtro=ano, mes_filtro=mes, data_filtro=data_relatorio).copy()
     except Exception as err:
         print(f"Unexpected {err=}, {type(err)=}")
         raise
@@ -355,8 +360,9 @@ def main(filePath:str, sheet_name:str, original_df: pd.DataFrame, clientes_list:
     #plot_df = filter_by_param(iter_df_date_filtered, 'Eficiência')
 
     #df = filter_by_param(df,param=parametro)
-    ### Carregar os dados dos responsaveis pelas ETE
-    filePath2 = r'H:\Meu Drive\_Lucas\Controle de Qualidade\Dados\dados_ete.xlsx'
+
+    ### LEMBRAR DE VERIFICAR O FILEPATH
+    filePath2 = r'G:\Meu Drive\_Lucas\Controle de Qualidade\Dados\dados_ete.xlsx'
     sheet_name2 = 'responsavel'
     df_responsavelETE = pd.read_excel(io=filePath2,sheet_name=sheet_name2)
     df_responsavelETE = df_responsavelETE.set_index('Cliente')
@@ -369,8 +375,8 @@ def main(filePath:str, sheet_name:str, original_df: pd.DataFrame, clientes_list:
         
         print(f"{cliente} sem responsavel")
     
-    ### Carregar os dados da descricao das ETE
-    filePath3 = r'H:\Meu Drive\_Lucas\Controle de Qualidade\Dados\dados_ete.xlsx'
+    ### LEMBRAR DE VERIFICAR O FILEPATH
+    filePath3 = r'G:\Meu Drive\_Lucas\Controle de Qualidade\Dados\dados_ete.xlsx'
     sheet_name3 = 'desc_ete_sheet'
     df_descETE = pd.read_excel(io=filePath3,sheet_name=sheet_name3)
     df_descETE = df_descETE.set_index('Cliente')
@@ -409,19 +415,12 @@ def main(filePath:str, sheet_name:str, original_df: pd.DataFrame, clientes_list:
                 data=data_str, ano=ano, hoje=get_date())
     
 if __name__ == "__main__":
-
-    #cliente="Alma Viva"
-    #parametro = "DBO"
-    
-    ###carregar os dados das ETE para fazer a tabela
-    filePath = r'H:\Meu Drive\_Lucas\Controle de Qualidade\Dados\Resultados ETE2.xlsx'
+  
+    ### LEMBRAR DE VERIFICAR O FILEPATH
+    filePath = r'G:\Meu Drive\_Lucas\Controle de Qualidade\Dados\Resultados ETE2.xlsx'
     sheet_name = 'Acompanhamento da ETE'
     original_df = pd.read_excel(io=filePath, sheet_name=sheet_name, header=1)
     clientes_list = original_df['Cliente'].unique()
-    
-    # mode 1 = Todos os clientes
-    # mode 2 = Cliente específico
-    
     run_mode = [0,1,2]
     
     while True:
@@ -437,6 +436,7 @@ if __name__ == "__main__":
             print('A opção selecionada não esta na lista de modos disponíveis')
             
         else:
+
             if resposta==0:
                 
                 exit()
@@ -464,6 +464,10 @@ if __name__ == "__main__":
                 msg_data_relatorio = 'Digite aqui o ano, mês e dia limite do(s) relatório(s) -> formato AAAA-MM: '
                 data_relatorio = input(msg_data_relatorio) or "2023-03"              
                 ano, mes = data_relatorio.split("-")
+                for i, cl in enumerate(clientes_list):
+                    print(f'{i} - {cl}')
+
+                #print(*clientes_list, sep='\n')
                 cliente = input('Digite aqui o nome do um cliente: ') or "Alto da Boa Vista"
                 
                 main(filePath=filePath, sheet_name=sheet_name, 
